450ea762466d106e850ebdd4be02b20b4dfc8785
cdfishe1 <75449671+cdfishe1@users.noreply.github.com>
2021-04-21 10:13:27 -0500

    Add adminstration to deptSeeds

--- deptSeeds.sql
+++ deptSeeds.sql
@@ -1,6 +1,6 @@
 USE employee_TrackerDB;
 
 INSERT INTO department (dept_name)
-VALUES ('Accounting'), ('Engineering'), ('Human Resources'), ('Maintenance');
+VALUES ('Accounting'), ('Administratiion'), ('Engineering'), ('Human Resources'), ('Maintenance');
 
 
