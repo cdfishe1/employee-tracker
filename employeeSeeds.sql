f9d9438b2c1f9838bf6a63e9b70dbfb7068625bb
cdfishe1 <75449671+cdfishe1@users.noreply.github.com>
2021-04-21 10:51:52 -0500

    add employeeSeeds

---
+++ employeeSeeds.sql
@@ -0,0 +1,4 @@
+USE employee_TrackerDB;
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Jose', 'Gonzalez', 1, 1), ('Jane', 'Liu', 2, 2), ('Alicia', 'Logan', 3, 3), ('Joshua', 'Brenner', 4, 4), ('Carol', 'Donaldson', 5, 5);
