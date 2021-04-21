6c6d90dc6f084c50072743f87d4732a4b12c72fc
cdfishe1 <75449671+cdfishe1@users.noreply.github.com>
2021-04-21 10:42:37 -0500

    Reconstruct roleSeeds to populate correctly

--- employeeTrackerSchema.sql
+++ employeeTrackerSchema.sql
@@ -16,7 +16,7 @@ CREATE TABLE role (
   id INT NOT NULL AUTO_INCREMENT,
   title VARCHAR(30) NULL,
   salary DECIMAL(8,2) NULL,
-  department_id INT NULL,
+  dept_id INT NULL,
   PRIMARY KEY (id)
 );
 
