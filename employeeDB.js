1a34538d51e1c37b734c25949da92d508d26502c
cdfishe1 <75449671+cdfishe1@users.noreply.github.com>
2021-04-21 11:41:56 -0500

    Add main menu with inquirer

--- employeeDB.js
+++ employeeDB.js
@@ -16,11 +16,76 @@ const connection = mysql.createConnection({
   database: 'employee_trackerdb',
 });
 
+const start = () => {
+    inquirer
+      .prompt({
+        name: 'action',
+        type: 'list',
+        message: 'What would you like to do?',
+        choices: [
+          'Add department',
+          'Add role',
+          'Add employee',
+          'View all employees',
+          'View all employees by department',
+          'View all employees by role',
+          'Update employee role',
+          'exit',
+        ],
+      })
+      .then((answer) => {
+        switch (answer.action) {
+          case 'Add department':
+            //addDept();
+            console.log(answer.action);
+            break;
+  
+          case 'Add role':
+            // addRole();
+            console.log(answer.action);
+            break;
+  
+          case 'Add Employee':
+            // addEmployee();
+            console.log(answer.action);
+            break;
+  
+          case 'View all employees':
+            // viewAllEmployees();
+            console.log(answer.action);
+            break;
 
+          case 'View all employees by department':
+            // viewAllEmployeesDept();
+            console.log(answer.action);
+            break;
+
+          case 'View all employees by role':
+            // viewAllEmployeesRole();
+            console.log(answer.action);
+            break;
+
+          case 'Update employee role':
+             // viewAllEmployeesRole();
+            console.log(answer.action);
+             break;
+  
+          case 'Exit':
+            connection.end();
+            break;
+  
+          default:
+            console.log(`Invalid action: ${answer.action}`);
+            break;
+        }
+      })
+    
+        
+  };
 
 
 connection.connect((err) => {
     if (err) throw err;
     console.log(`connected as id ${connection.threadId}\n`);
-    // start();
+    start();
   });
